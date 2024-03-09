aeddf2d13ed59cd10e949aba9bd5f8afffe7daf1
yurii <robka4an@gmail.com>
2024-03-09 16:31:27 +0200

    update start js

---
+++ src/js/1.js
@@ -0,0 +1,25 @@
+// function makeTransaction(quantity, pricePerDroid) {
+//   const totalPrice = quantity * pricePerDroid;
+//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// }
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+// function getShippingMessage(country, price, deliveryFee) {
+//   const totalPrice = price + deliveryFee;
+//   return `Shipping to ${country} will cost ${totalPrice} credits`;
+// }
+// console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
+// console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+// function getElementWidth(content, padding, border) {
+//   const contentFloat = Number.parseFloat(content);
+//   const paddingFloat = Number.parseFloat(padding);
+//   const borderFloat = Number.parseFloat(border);
+//   return contentFloat + paddingFloat * 2 + borderFloat * 2;
+// }
+// console.log(getElementWidth('50px', '8px', '4px')); // 74
+// console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+// console.log(getElementWidth('200px', '0px', '0px')); // 200
--- src/main.js
+++ src/main.js
@@ -1,12 +1,1 @@
-import Swiper from 'swiper';
-// import 'swiper/css';
-// import { accordion } from './js/acord';
-// import './js/acord';
-
-// ================================================================= ^ accord ^
-const swiper = new Swiper('.mySwiper', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+import './js/1';
