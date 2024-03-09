fb416c1a1786dcc312392287a03373124c7189c5
yurii <robka4an@gmail.com>
2024-03-09 14:09:28 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Swiper from 'swiper';
 // import 'swiper/css';
 // import { accordion } from './js/acord';
-import './js/acord';
+// import './js/acord';
 
 // ================================================================= ^ accord ^
 const swiper = new Swiper('.mySwiper', {
