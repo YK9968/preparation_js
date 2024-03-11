b93960790c463c3e9bc1001e4af29fd2e75bf8e1
yurii <robka4an@gmail.com>
2024-03-11 16:17:12 +0200

    finish five modul

--- src/js/1.js
+++ src/js/1.js
@@ -5,7 +5,7 @@
 // console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 // console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 // console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-
+// --------->
 // function getShippingMessage(country, price, deliveryFee) {
 //   const totalPrice = price + deliveryFee;
 //   return `Shipping to ${country} will cost ${totalPrice} credits`;
@@ -13,7 +13,7 @@
 // console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
 // console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
 // console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
-
+// --------->
 // function getElementWidth(content, padding, border) {
 //   const contentFloat = Number.parseFloat(content);
 //   const paddingFloat = Number.parseFloat(padding);
@@ -38,7 +38,7 @@
 // console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
 // console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
 // console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
-
+// --------->
 // function formatMessage(message, maxLength) {
 //   if (message.length === maxLength) {
 //     return message;
@@ -52,7 +52,7 @@
 // console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
 // console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
 // console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
+// --------->
 // function checkForSpam(message) {
 //   if (
 //     message.toLowerCase().includes('spam') ||
@@ -70,7 +70,7 @@
 // console.log(checkForSpam('Trust me, this is not a spam message')); // true
 // console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
 // console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
-
+// --------->
 // function getShippingCost(country) {
 //   let price;
 //   switch (country) {
@@ -99,3 +99,349 @@
 // console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
 
 // ======================================================== ^ hw 2 ^ ===============================
+// function slugify(title) {
+//   return title.toLowerCase().split(' ').join('-');
+// }
+// console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+// console.log(slugify('English for developer')); // "english-for-developer"
+// console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+// console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+// --------->
+// function makeArray(firsrArray, secondArray, maxLength) {
+//   const thirtArray = firsrArray.concat(secondArray);
+//   return thirtArray.slice(0, maxLength);
+// }
+// console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+// console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+// console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
+// --------->
+// function filterArray(numbers, value) {
+//   const bigValue = [];
+//   for (let number of numbers) {
+//     if (number > value) {
+//       bigValue.push(number);
+//     }
+//   }
+//   return bigValue;
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+// ======================================================== ^ hw 3 ^ ===============================
+// function isEnoughCapacity(products, containerSize) {
+//   let totalProducts = 0;
+//   for (let product of Object.values(products)) {
+//     totalProducts += product;
+//   }
+
+//   if (totalProducts < containerSize) {
+//     return true;
+//   }
+//   return false;
+// }
+// console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+// console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+// console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+// console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+// --------->
+// function calcAverageCalories(days) {
+//   let totalCalories = 0;
+//   for (let day of days) {
+//     totalCalories += day.calories;
+//   }
+//   if (days.length > 0) {
+//     return totalCalories / days.length;
+//   }
+//   return 0;
+// }
+// console.log(
+//   calcAverageCalories([
+//     { day: 'monday', calories: 3010 },
+//     { day: 'tuesday', calories: 3200 },
+//     { day: 'wednesday', calories: 3120 },
+//     { day: 'thursday', calories: 2900 },
+//     { day: 'friday', calories: 3450 },
+//     { day: 'saturday', calories: 3280 },
+//     { day: 'sunday', calories: 3300 },
+//   ])
+// ); // 3180
+
+// console.log(
+//   calcAverageCalories([
+//     { day: 'monday', calories: 2040 },
+//     { day: 'tuesday', calories: 2270 },
+//     { day: 'wednesday', calories: 2420 },
+//     { day: 'thursday', calories: 1900 },
+//     { day: 'friday', calories: 2370 },
+//     { day: 'saturday', calories: 2280 },
+//     { day: 'sunday', calories: 2610 },
+//   ])
+// ); // 2270
+
+// console.log(calcAverageCalories([])); // 0
+// --------->
+// const profile = {
+//   username: 'Jacob',
+//   playTime: 300,
+//   changeUsername(newName) {
+//     this.username = newName;
+//   },
+//   updatePlayTime(hours) {
+//     this.playTime += hours;
+//   },
+//   getInfo() {
+//     return `${this.username} has ${this.playTime} active hours!`;
+//   },
+// };
+
+// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+// profile.changeUsername('Marco');
+// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+// profile.updatePlayTime(20);
+// console.log(profile.getInfo()); // "Marco has 320 active hours!"
+// ======================================================== ^ hw 4 ^ ===============================
+// const getUserNames = users => {
+//   return users.map(user => user.name);
+// };
+// console.log(
+//   getUserNames([
+//     {
+//       name: 'Moore Hensley',
+//       email: 'moorehensley@indexia.com',
+//       balance: 2811,
+//     },
+//     {
+//       name: 'Sharlene Bush',
+//       email: 'sharlenebush@tubesys.com',
+//       balance: 3821,
+//     },
+//     {
+//       name: 'Ross Vazquez',
+//       email: 'rossvazquez@xinware.com',
+//       balance: 3793,
+//     },
+//     {
+//       name: 'Elma Head',
+//       email: 'elmahead@omatom.com',
+//       balance: 2278,
+//     },
+//     {
+//       name: 'Carey Barr',
+//       email: 'careybarr@nurali.com',
+//       balance: 3951,
+//     },
+//     {
+//       name: 'Blackburn Dotson',
+//       email: 'blackburndotson@furnigeer.com',
+//       balance: 1498,
+//     },
+//     {
+//       name: 'Sheree Anthony',
+//       email: 'shereeanthony@kog.com',
+//       balance: 2764,
+//     },
+//   ])
+// ); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+// --------->
+
+// const getUsersWithFriend = (users, friendName) => {
+//   return users.filter(user => user.friends.includes(friendName));
+// };
+// const allUsers = [
+//   {
+//     name: 'Moore Hensley',
+//     friends: ['Sharron Pace'],
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//   },
+//   {
+//     name: 'Elma Head',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//   },
+//   {
+//     name: 'Carey Barr',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//   },
+// ];
+
+// console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
+// // [
+// //   {
+// //     name: "Sharlene Bush",
+// //     friends: ["Briana Decker", "Sharron Pace"]
+// //   },
+// //   {
+// //     name: "Sheree Anthony",
+// //     friends: ["Goldie Gentry", "Briana Decker"]
+// //   }
+// // ]
+
+// console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
+// // [
+// //   {
+// //     name: "Elma Head",
+// //     friends: ["Goldie Gentry", "Aisha Tran"]
+// //   },
+// //   {
+// //     name: "Sheree Anthony",
+// //     friends: ["Goldie Gentry", "Briana Decker"]
+// //   }
+// // ]
+
+// console.log(getUsersWithFriend(allUsers, 'Adrian Cross')); // []
+// --------->
+// const sortByDescendingFriendCount = users => {
+//   return users.toSorted((a, b) => b.friends.length - a.friends.length);
+// };
+// console.log(
+//   sortByDescendingFriendCount([
+//     {
+//       name: 'Moore Hensley',
+//       friends: ['Sharron Pace'],
+//       gender: 'male',
+//     },
+//     {
+//       name: 'Sharlene Bush',
+//       friends: ['Briana Decker', 'Sharron Pace'],
+//       gender: 'female',
+//     },
+//     {
+//       name: 'Ross Vazquez',
+//       friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//       gender: 'male',
+//     },
+//     {
+//       name: 'Elma Head',
+//       friends: ['Goldie Gentry', 'Aisha Tran'],
+//       gender: 'female',
+//     },
+//     {
+//       name: 'Carey Barr',
+//       friends: ['Jordan Sampson', 'Eddie Strong'],
+//       gender: 'male',
+//     },
+//     {
+//       name: 'Blackburn Dotson',
+//       friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//       gender: 'male',
+//     },
+//     {
+//       name: 'Sheree Anthony',
+//       friends: ['Goldie Gentry', 'Briana Decker'],
+//       gender: 'female',
+//     },
+//   ])
+// );
+// // [
+// //   {
+// //     name: "Ross Vazquez",
+// //     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+// //     gender: "male"
+// //   },
+// //   {
+// //     name: "Sharlene Bush",
+// //     friends: ["Briana Decker", "Sharron Pace"],
+// //     gender: "female"
+// //   },
+// //   {
+// //     name: "Elma Head",
+// //     friends: ["Goldie Gentry", "Aisha Tran"],
+// //     gender: "female"
+// //   },
+// //   {
+// //     name: "Carey Barr",
+// //     friends: ["Jordan Sampson", "Eddie Strong"],
+// //     gender: "male"
+// //   },
+// //   {
+// //     name: "Blackburn Dotson",
+// //     friends: ["Jacklyn Lucas", "Linda Chapman"],
+// //     gender: "male"
+// //   },
+// //   {
+// //     name: "Sheree Anthony",
+// //     friends: ["Goldie Gentry", "Briana Decker"],
+// //     gender: "female"
+// //   },
+// //   {
+// //     name: "Moore Hensley",
+// //     friends: ["Sharron Pace"],
+// //     gender: "male"
+// //   }
+// // ]
+// -------->
+
+// const getTotalBalanceByGender = (users, gender) => {
+//   return users
+//     .filter(user => user.gender === gender)
+//     .reduce((totalBalance, user) => {
+//       return (totalBalance += user.balance);
+//     }, 0);
+// };
+// const clients = [
+//   {
+//     name: 'Moore Hensley',
+//     gender: 'male',
+//     balance: 2811,
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     gender: 'female',
+//     balance: 3821,
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     gender: 'male',
+//     balance: 3793,
+//   },
+//   {
+//     name: 'Elma Head',
+//     gender: 'female',
+//     balance: 2278,
+//   },
+//   {
+//     name: 'Carey Barr',
+//     gender: 'male',
+//     balance: 3951,
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     gender: 'male',
+//     balance: 1498,
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     gender: 'female',
+//     balance: 2764,
+//   },
+// ];
+
+// console.log(getTotalBalanceByGender(clients, 'male')); // 12053
+
+// console.log(getTotalBalanceByGender(clients, 'female')); // 8863
+// ======================================================== ^ hw 5 ^ ===============================
