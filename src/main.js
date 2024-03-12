1a0a92bceb86eea54defde3e346faafee4a6daa4
yurii <robka4an@gmail.com>
2024-03-12 16:48:34 +0200

    add swiper

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-// import Swiper from 'swiper/bundle';
-// import 'swiper/css/bundle';
-// const swiper = new Swiper('.swiper', {
-//   effect: 'cube',
-//   cubeEffect: {
-//     slideShadows: false,
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-//   loop: true,
-// });
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  loop: true,
+});
