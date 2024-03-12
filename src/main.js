24b6a3e2066379bd4dd1aba772b4442fca22af9a
yurii <robka4an@gmail.com>
2024-03-12 15:57:20 +0200

    testing

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-import './js/1';
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
+// const swiper = new Swiper('.swiper', {
+//   effect: 'cube',
+//   cubeEffect: {
+//     slideShadows: false,
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   loop: true,
+// });
