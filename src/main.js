1ad92019b577c67fa40be19c04cda71a3cba21d7
yurii <robka4an@gmail.com>
2024-03-13 18:38:32 +0200

    start projects

--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
-const swiper = new Swiper('.swiper', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  loop: true,
-});
+
